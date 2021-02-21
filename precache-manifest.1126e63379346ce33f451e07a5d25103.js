80df9112602aacfcfaccfda08b8ee1cc8ba3510c
Amine <toptech6@gmail.com>
2021-02-21 22:39:28 +0100

    Updates

---
+++ precache-manifest.1126e63379346ce33f451e07a5d25103.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6beb7325afab3460bd543947ccf3dd6",
+    "url": "git@github.com:amine4000/Commy.git/index.html"
+  },
+  {
+    "revision": "887d3d608f1932c332f3",
+    "url": "git@github.com:amine4000/Commy.git/static/css/main.5091319f.chunk.css"
+  },
+  {
+    "revision": "e96cdb0810303ad95b5e",
+    "url": "git@github.com:amine4000/Commy.git/static/js/2.af4ae112.chunk.js"
+  },
+  {
+    "revision": "887d3d608f1932c332f3",
+    "url": "git@github.com:amine4000/Commy.git/static/js/main.701052d9.chunk.js"
+  },
+  {
+    "revision": "5e956a852f0cfcbb47b6",
+    "url": "git@github.com:amine4000/Commy.git/static/js/runtime~main.b6558cf8.js"
+  },
+  {
+    "revision": "f803d26bc12f10d623b72755226fea3f",
+    "url": "git@github.com:amine4000/Commy.git/static/media/logo.f803d26b.png"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "git@github.com:amine4000/Commy.git/static/media/shopping-bag.30f07071.svg"
+  }
+]);
